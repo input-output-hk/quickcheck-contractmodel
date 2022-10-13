c3fd8b8130175c11e8841be9ff0f910fa3db68cc
Ulf Norell <ulf.norell@gmail.com>
2022-10-13 15:40:46 +0200

    remove Hacks and depend on fixed cardano-api

--- contractmodel/src/Test/QuickCheck/ContractModel/Internal/Common.hs
+++ contractmodel/src/Test/QuickCheck/ContractModel/Internal/Common.hs
@@ -2,8 +2,6 @@ module Test.QuickCheck.ContractModel.Internal.Common (Era, era) where
 
 import Cardano.Api
 
-import Test.QuickCheck.ContractModel.Internal.Hacks()
-
 type Era = BabbageEra
 
 era :: ShelleyBasedEra Era
--- contractmodel/src/Test/QuickCheck/ContractModel/Internal/Hacks.hs
+++
@@ -1,7 +0,0 @@
-module Test.QuickCheck.ContractModel.Internal.Hacks where
-
-import Cardano.Api
-
--- TODO: this *needs* to be upstreamed - these implementations don't work and are
--- only here to make the type checker happy!
-instance Ord (AddressInEra BabbageEra)

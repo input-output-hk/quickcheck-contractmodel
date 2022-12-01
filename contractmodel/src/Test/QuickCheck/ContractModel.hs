4e435a6f8f4fbcf8d5f067f267bb8a8db3366f8c
Ulf Norell <ulf.norell@gmail.com>
2022-12-01 11:35:57 +0100

    complete exports in ContractModel module

--- contractmodel/src/Test/QuickCheck/ContractModel.hs
+++ contractmodel/src/Test/QuickCheck/ContractModel.hs
@@ -1,25 +1,55 @@
 module Test.QuickCheck.ContractModel
-  ( -- * The safe interface to `SymToken`
+  ( ContractModel(..)
+  , RunModel(..)
+  , IsRunnable(..)
+  , RunMonad(..)
+  , Actions
+  , Act(..)
+  , pattern Actions
+  , pattern ContractAction
+  , stateAfter
+  , runContractModel
+  , liftRunMonad
+  , contractState
+  , registerToken
+    -- * Chain index
+  , HasChainIndex(..)
+  , ChainIndex(..)
+  , ChainState(..)
+  , TxInState(..)
+  , Era
+    -- * The safe interface to `SymToken`
     --
     -- NOTE: we don't export the internals here because
     -- it's important that you can't tell the difference
     -- between differnelty numbered SymTokens as these are
     -- not guaranteed to be stable.
-    SymToken
+  , SymToken
   , SymValue
   , symIsZero
   , symLeq
   , toValue
   , toSymVal
   , inv
+  , SymValueLike(..)
   , TokenLike(..)
+  , HasSymTokens(..)
+    -- * Properties
+  , BalanceChangeOptions(..)
+  , assertBalanceChangesMatch
+  , signerPaysFees
+  , asserts
+    -- * Dynamic logic
+  , module DL
     -- * The safe interface to `Spec`
     --
     -- NOTE: we don't export internals here because we
     -- really don't want people seeing or changing the
     -- sensitive parts of the model.
-  , Spec
+  , ModelState
+  , Spec(..)
   , GetModelState(..)
+  , runSpec
   , currentSlot
   , balanceChanges
   , balanceChange
@@ -30,13 +60,25 @@ module Test.QuickCheck.ContractModel
   , askContractState
   , viewModelState
   , viewContractState
+  , createToken
   , mint
   , burn
   , deposit
   , withdraw
   , transfer
+  , waitUntil
+  , wait
   , assertSpec
+  , coerceSpec
+    -- * Internals
+  , fromStateModelActions
   ) where
 
+import Test.QuickCheck.ContractModel.Internal
+import Test.QuickCheck.ContractModel.Internal.ChainIndex
+import Test.QuickCheck.ContractModel.Internal.Common
+import Test.QuickCheck.ContractModel.Internal.Model
 import Test.QuickCheck.ContractModel.Internal.Spec
 import Test.QuickCheck.ContractModel.Internal.Symbolics
+
+import Test.QuickCheck.ContractModel.DL as DL

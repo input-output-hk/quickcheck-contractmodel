36ea2e91d815e7a092e1254e0dbe7b64b6f1756f
zeme <zem1@pm.me>
2023-02-07 22:29:54 +0700

    WIP

--- nix/contractmodel/devshells.nix
+++ nix/contractmodel/devshells.nix
@@ -1,20 +1,8 @@
 { inputs, cell }: 
-let 
-  pkgs = import inputs.nixpkgs {};
-in 
 {
   default = inputs.devx.devShells.ghc8107.overrideAttrs (attrs: {
     buildInputs = attrs.buildInputs ++ [
       inputs.nixpkgs.lzma.dev
     ];
   });
-
-  # TODO uncomment once devx has been standardized 
-  # extended = inputs.std.lib.dev.mkShell {
-  #   name = "qc-c";
-  #   imports = [ inputs.devx.devshellProfiles.ghc8107 ];
-  #   packages = [
-  #     # Custom stuff 
-  #   ];
-  # };
 }

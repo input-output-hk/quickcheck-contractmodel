27fed430b3e34989ffdb8993a34e9dca190e5a75
zeme <zem1@pm.me>
2023-02-07 22:26:51 +0700

    Add lzma-dev to the shell

--- nix/contractmodel/devshells.nix
+++ nix/contractmodel/devshells.nix
@@ -1,6 +1,13 @@
 { inputs, cell }: 
+let 
+  pkgs = import inputs.nixpkgs {};
+in 
 {
-  default = inputs.devx.devShells.ghc8107; 
+  default = inputs.devx.devShells.ghc8107.overrideAttrs (attrs: {
+    buildInputs = attrs.buildInputs ++ [
+      inputs.nixpkgs.lzma.dev
+    ];
+  });
 
   # TODO uncomment once devx has been standardized 
   # extended = inputs.std.lib.dev.mkShell {

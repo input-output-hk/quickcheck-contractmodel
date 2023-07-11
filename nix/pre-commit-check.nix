b2c910db316d1e0496d7e161816108cd6eb201eb
zeme <lorenzo.calegari@iohk.io>
2023-07-12 01:12:14 +0200

    WIP

--- nix/pre-commit-check.nix
+++ nix/pre-commit-check.nix
@@ -3,9 +3,7 @@
 
 {
   cabal-fmt.enable = true;
-
   nixpkgs-fmt.enable = true;
-
   fourmolu.enable = true;
   fourmolu.extraOptions = "-o -XImportQualifiedPost -o -XTypeApplications -o -XPatternSynonyms";
 }

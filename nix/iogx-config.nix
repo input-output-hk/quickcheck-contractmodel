c4e1c184b35a6784a1d9df17a51d12064dec4d45
zeme <lorenzo.calegari@iohk.io>
2023-08-22 08:50:11 +0200

    WIP

--- nix/iogx-config.nix
+++ nix/iogx-config.nix
@@ -1,5 +1,5 @@
 # This file is part of the IOGX template and is documented at the link below:
-# https://www.github.com/input-output-hk/iogx#32-nixiogx-confignix
+# https://www.github.com/input-output-hk/iogx#32-nixhaskellnix
 
 {
   systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];

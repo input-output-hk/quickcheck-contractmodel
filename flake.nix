7ee1607b0263e3c1b27b26a4f2a52e40119dc2bd
zeme-wana <15709674+zeme-wana@users.noreply.github.com>
2023-11-08 15:43:01 +0100

    Bump IOGX v4 (#30)
    * Bump IOGX v4

--- flake.nix
+++ flake.nix
@@ -1,17 +1,32 @@
-# This file is part of the IOGX template and is documented at the link below:
-# https://www.github.com/input-output-hk/iogx#31-flakenix
-
 {
   description = "Interface to quickcheck-dynamic for Developers of Plutus Scripts";
 
 
   inputs = {
-    iogx.url = "github:input-output-hk/iogx";
-    iogx.inputs.hackage.follows = "hackage";
+
+    iogx = {
+      url = "github:input-output-hk/iogx";
+      inputs.hackage.follows = "hackage";
+      inputs.CHaP.follows = "CHaP";
+      inputs.haskell-nix.follows = "haskell-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixpkgs.follows = "haskell-nix/nixpkgs";
 
     hackage = {
       url = "github:input-output-hk/hackage.nix";
       flake = false;
+    };
+
+    CHaP = {
+      url = "github:input-output-hk/cardano-haskell-packages?ref=repo";
+      flake = false;
+    };
+
+    haskell-nix = {
+      url = "github:input-output-hk/haskell.nix";
+      inputs.hackage.follows = "hackage";
     };
   };
 
@@ -19,20 +34,18 @@
   outputs = inputs: inputs.iogx.lib.mkFlake {
     inherit inputs;
     repoRoot = ./.;
-    systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    systems = [ "x86_64-darwin" "x86_64-linux" "aarch64-darwin" ];
+    outputs = import ./nix/outputs.nix;
   };
 
 
   nixConfig = {
-
     extra-substituters = [
       "https://cache.iog.io"
     ];
-
     extra-trusted-public-keys = [
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
     ];
-
     allow-import-from-derivation = true;
   };
 }
--- nix/formatters.nix
+++
@@ -1,9 +0,0 @@
-# This file is part of the IOGX template and is documented at the link below:
-# https://www.github.com/input-output-hk/iogx#38-nixformattersnix
-
-{
-  nixpkgs-fmt.enable = true;
-  cabal-fmt.enable = false;
-  fourmolu.enable = false;
-  fourmolu.extraOptions = "-o -XImportQualifiedPost -o -XTypeApplications -o -XPatternSynonyms";
-}
